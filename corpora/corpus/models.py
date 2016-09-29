6a1bc3f2ddea3434eb02cb0207d7f6f34202ffb8
Alex Adkins <adkins.alexandra@gmail.com>
2016-09-29 14:38:15 +1300

    ADD: Initial models

--- corpora/corpus/models.py
+++ corpora/corpus/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Sentence(models.Model):
+	text = models.CharField(help_text='The sentence to be spoken.',max_length=250)
+	# language
+
+class Recording(models.Model):
+    person = models.ForeignKey('website.Person')
+    sentence = models.ForeignKey(Sentence)
+    audio_file = models.FileField()
--- corpora/people/models.py
+++ corpora/people/models.py
@@ -2,4 +2,36 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Tribe(models.Model):
+    name = models.CharField(help_text='Name',max_length=200)
+
+    class Meta:
+        verbose_name = 'Tribe'
+        verbose_name_plural = 'Tribes'
+
+
+class Demographic(models.Model):
+	SEX_CHOICES = [
+		('M', 'Male'),
+		('F', 'Female')
+	]
+
+	age = models.IntegerField(help_text='Age')
+	sex = models.ChoiceField(help_text='Gender', choices=SEX_CHOICES)
+	# tribe
+	# ethnicities
+
+
+class Person(models.Model):
+	first_name = models.CharField(help_text='First Name', max_length=200)
+    last_name = models.CharField(help_text='Last Name', max_length=200)
+    email = models.EmailField(help_text='Email', max_length=200, unique=True, null=True, default=None, blank=True)
+    demographic = models.OneToOneField(Demographic, on_delete=models.CASCADE, null=True)
+    # languages
+
+    def name(self):
+        return self.first_name + ' ' + self.last_name
+
+    class Meta:
+        verbose_name = 'Person'
