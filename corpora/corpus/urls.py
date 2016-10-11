b8462e8b2feedc5987a6019becec69d53a9d5f2d
Alex Adkins <adkins.alexandra@gmail.com>
2016-10-12 10:20:27 +1300

    Recording and playback in browser; Creating & saving recording model when audio file submitted by user

--- corpora/corpus/urls.py
+++ corpora/corpus/urls.py
@@ -4,5 +4,6 @@ from corpus import views
 
 urlpatterns = [
 	url(r'^$', views.record, name='record'),
-	url(r'^submit_recording/$', views.submit_recording, name='submit_recording'),
+	url(r'^submit_recording/', views.submit_recording, name='submit_recording'),
+	url(r'^failed_submit/', views.failed_submit, name='failed_submit'),
 ]
