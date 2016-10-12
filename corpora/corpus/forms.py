1e408716d44f05b37ec77a428c9a79b9973eaf03
Alex Adkins <adkins.alexandra@gmail.com>
2016-10-12 16:11:19 +1300

    IMPROVE: Replace getUserMedia with recorderJS for improved audio files

--- corpora/corpus/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from corpus.models import Recording
-
-
-class RecordingForm(forms.ModelForm):
-	class Meta:
-		model = Recording
-		fields = ('audio_file',)
--- corpora/corpus/static/corpus/js/record.js
+++ corpora/corpus/static/corpus/js/record.js
@@ -1,97 +1,85 @@
-navigator.getUserMedia = navigator.getUserMedia ||
-	navigator.webkitGetUserMedia ||
-	navigator.mozGetUserMedia;
-
-var record = document.querySelector('.record-button');
-var play = document.querySelector('.play-button');
+var record_button = document.querySelector('.record-button');
+var play_button = document.querySelector('.play-button');
 var audio = document.querySelector('.play-audio');
 var approve = document.getElementById('approve');
 
-var audioBlob;
+var audioBlob, fileName;
 
-if (navigator.getUserMedia) {
-	// Load getUserMedia API
-	navigator.getUserMedia( 
-		// Constraints - only audio needed for this app
-		{
-			audio: true
-		},
-
-		// Success callback
-		function (stream) {
-			// Initilize 
-			var mediaRecorder = new MediaRecorder(stream);
-			
-			// Record or halt recording when pressing record button
-			record.onclick = function() {
-				if (mediaRecorder.state == 'inactive') {
-					// Start recorder if inactive
-					mediaRecorder.start();
-					$('.foreground-circle.record').removeClass('unclicked-circle').addClass('clicked-circle');	
-				} else {
-					// Stop recorder if active
-					mediaRecorder.stop();
-					$('.foreground-circle.record').removeClass('clicked-circle').addClass('unclicked-circle');	
-				}
-				
-			}
-
-			// Store audio data as available
-			var chunks = [];
-			mediaRecorder.ondataavailable = function(e) {
-				chunks.push(e.data);
-			}
-
-			// Generate audio file & url upon recording stopped
-			mediaRecorder.onstop = function(e) {
-				// Save audio chunks to Javascript Audio Blob
-				audioBlob = new Blob(chunks, {'type' : 'audio/wav'});
-
-				// Flush the stored data
-				chunks = [];
-
-				// Generate audio file URL for playback
-				var audioURL = window.URL.createObjectURL(audioBlob);
-				audio.src = audioURL;
-			}
-
-			// If play button clicked, play audio
-			play.onclick = function(){
-				audio.play();
-			}
-
-			// If "approve audio" button clicked, create formdata to save recording model
-			approve.onclick = function(){
-				// Initialize FormData
-				var fd = new FormData();
-				// Set enctype to multipart; necessary for audio form data
-				fd.enctype="multipart/form-data";
-
-				// Add audio blob as blob.wav to form data
-				fd.append('audio_file', audioBlob, "blob.wav");
-
-				// Append necessary person and sentence pks to form data to add to recording model
-				fd.append('person', person_pk);
-				fd.append('sentence', sentence_pk);
-
-				// Send ajax POST request back to corpus/views.py
-				$.ajax({
-					type: 'POST',
-					url: '/',
-					data: fd,
-					processData: false,
-					contentType: false
-				}).done(function(data) {
-					console.log("Recording data submitted and saved");
-				})
-			}
-		},
-
-		// Error callback
-		function (err) {
-			console.log('The following gUM error occurred: ' + err);
-		}
-	);
+// Check if recorderjs supported
+// TODO: Make recording dependent on whether recorderjs is supported or not
+if (!Recorder.isRecordingSupported()) {
+	console.log("Recorder not supported");
 } else {
-	console.log('getUserMedia not supported on this browser.');
+	console.log("Recorder supported");
+}
+
+// Initialize the recorder
+// Using recorderjs: https://github.com/chris-rudmin/Recorderjs
+// encoderPath option: directs to correct encoderWorker location
+// leaveStreamOpen option: allows for recording multiple times wihtout reinitializing audio stream
+var recorder = new Recorder({
+	encoderPath: encoder_path,
+	leaveStreamOpen: true
+});
+
+var recording = false;
+// Record or halt recording when pressing record button
+record_button.onclick = function() {
+	if (recording == false) {
+		// Start recorder if inactive and set recording state to true
+		recording = true
+		recorder.start();
+
+		$('.foreground-circle.record').removeClass('unclicked-circle').addClass('clicked-circle');	
+	} else {
+		// Stop recorder if active and set recording state to false
+		recording = false
+		recorder.stop();
+
+		$('.foreground-circle.record').removeClass('clicked-circle').addClass('unclicked-circle');	
+	}
+	
+}
+
+// Have recorder listen for when the data is available
+recorder.addEventListener("dataAvailable", function(e) {
+	audioBlob = new Blob( [e.detail], {type: 'audio/ogg'});
+	fileName = new Date().toISOString() + ".ogg";
+	var audioURL = URL.createObjectURL( audioBlob );
+
+	audio.src = audioURL;
+});
+
+// If play button clicked, play audio
+play_button.onclick = function(){
+	audio.play();
+}
+
+// Initialize audio stream (and ask the user if recording allowed?)
+recorder.initStream();
+
+// If "approve audio" button clicked, create formdata to save recording model
+approve.onclick = function(){
+	// Initialize FormData
+	var fd = new FormData();
+	// Set enctype to multipart; necessary for audio form data
+	fd.enctype="multipart/form-data";
+
+	// Add audio blob as blob.wav to form data
+	fd.append('audio_file', audioBlob, fileName);
+
+	// Append necessary person and sentence pks to form data to add to recording model
+	fd.append('person', person_pk);
+	fd.append('sentence', sentence_pk);
+
+	// Send ajax POST request back to corpus/views.py
+	$.ajax({
+		type: 'POST',
+		url: '/',
+		data: fd,
+		processData: false,
+		contentType: false
+	}).done(function(data) {
+		console.log("Recording data submitted and saved");
+	})
 }
