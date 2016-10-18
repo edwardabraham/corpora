4cd48eb9f2b0965ee310ed56b84e1d3b28c38cc5
Alex Adkins <adkins.alexandra@gmail.com>
2016-10-18 17:03:32 +1300

    Login implemented

--- corpora/corpora/views.py
+++ corpora/corpora/views.py
@@ -1,9 +1,23 @@
 from django.template.context import RequestContext
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
 
 from corpus import views
 
 def login(request):
+	# if request.user.is_authenticated():
+	# 	return redirect('corpus:record')
+
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			auth_login(request, user)
+			print "LOGGED IN SUCCESSFULLY"
+		else:
+			print "INVALID LOGIN"
+
 	return render(request, 'corpora/login.html')
 
 def home(request):
