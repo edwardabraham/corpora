0173a35d5d47e2cb1d90b05f1d4768dfb0a58efa
Alex Adkins <adkins.alexandra@gmail.com>
2016-10-18 16:44:59 +1300

    ADD: Button behavior on hover

--- corpora/corpora/static/corpora/js/transitions.js
+++ corpora/corpora/static/corpora/js/transitions.js
@@ -2,20 +2,32 @@ function nextPageTransition(currPage, nextPage) {
 	currPage.addClass("pt-page-moveToLeft pt-page-fadeOut");
 	nextPage.addClass("pt-page-current pt-page-moveFromRight");
 
-	currPage.one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",   
-		function(e) {
-			nextPage.removeClass("pt-page-moveFromRight");
-			currPage.removeClass("pt-page-current pt-page-moveToLeft pt-page-fadeOut");
+	nextPage.find(".button").show();
+
+	currPage.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",   
+		function(event) {
+			if (event.target.id == currPage.attr('id')) {
+				nextPage.removeClass("pt-page-moveFromRight");
+				currPage.removeClass("pt-page-current pt-page-moveToLeft pt-page-fadeOut");
+
+				currPage.find(".button").hide();
+			};
 	});
 }
 
 function prevPageTransition(currPage, prevPage) {
 	currPage.addClass("pt-page-moveToRight pt-page-fadeOut");
 	prevPage.addClass("pt-page-current pt-page-moveFromLeft");
+
+	prevPage.find(".button").show();
 	
-	currPage.one("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
-		function(e) {
-			prevPage.removeClass("pt-page-moveFromLeft");
-			currPage.removeClass("pt-page-current pt-page-moveToRight pt-page-fadeOut");
+	currPage.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
+		function(event) {
+			if (event.target.id == currPage.attr('id')) {
+				prevPage.removeClass("pt-page-moveFromLeft");
+				currPage.removeClass("pt-page-current pt-page-moveToRight pt-page-fadeOut");
+
+				currPage.find(".button").hide();
+			};
 	});
 }
