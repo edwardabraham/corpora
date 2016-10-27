26628a50344c7094e0a258aa06fd2afde5903e42
admin <admin@Pony.local>
2016-10-27 20:15:17 +1300

    Added proper config for allowed hosts.

--- corpora/corpora/settings.py
+++ corpora/corpora/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ['DJANGO_ISNOT_PRODUCTION'])
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(' ')
 
 # Application definition
 
