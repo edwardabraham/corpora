f8fc6ff472768824427b5c29df8abc1d5c263c27
Keoni Mahelona <keoni@kmahelona.net>
2016-11-29 19:07:35 +1300

    1. Changed django hosts.

--- corpora/corpora/settings.py
+++ corpora/corpora/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ['DJANGO_ISNOT_PRODUCTION'])
 
-# ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(' ')
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(' ')
 
 # Application definition
 
